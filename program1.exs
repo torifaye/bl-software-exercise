045423c18fad452e8690350214bc56f98a250a41
Bobby Yost <bobby.9202@gmail.com>
2023-12-03 01:32:26 -0500

    Implement all of functionality in program2.exs
    * Implement three-list-reduction function
    * Add function documentation to all public functions
    * Add typespecs to all functions

--- program1.exs
+++ program1.exs
@@ -1,8 +1,20 @@
 defmodule M do
+  @doc """
+  Reads everything provided to program from standard input
+  """
+  @spec read() :: chardata() | nodata()
   def read do
     IO.read(:stdio, :all)
   end
 
+  @doc """
+  Converts raw string of integers, each on a new line, into a List
+
+  ## Examples
+  iex> M.input_to_list("+5\n-3\n+4")
+  [5, -3, 4]
+  """
+  @spec input_to_list(String.t()) :: list()
   def input_to_list(input) do
     input
     |> String.split()
@@ -10,6 +22,14 @@ defmodule M do
     |> Enum.map(&elem(&1, 0))
   end
 
+  @doc """
+  Sums up all values in provided list.
+
+  ## Examples
+  iex> M.calculate([5,-3,4], 0)
+  6
+  """
+  @spec calculate(list(), integer()) :: integer()
   def calculate([head | tail], acc) do
     calculate(tail, acc + head)
   end
